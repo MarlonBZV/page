b744e590ee391b1c77134b98f0226b99e37aedea
MarlonBZV <marlonzevallos29@gmail.com>
2023-04-12 09:33:07 -0500

    2

---
+++ swiper.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+    // Opciones de Swiper
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
